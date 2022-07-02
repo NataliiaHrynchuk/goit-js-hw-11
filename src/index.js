48c2b7a52e21846d987b78d17ffb4eb7f1263e4c
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-07-02 16:26:30 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import {fetchImages} from './fetchImages';
+let searchQuery = '';
+let page = 1;
+const refs = {
+    form: document.querySelector('#search-form'),
+    loadMoreBtn: document.querySelector('.load-more'),
+};
+refs.loadMoreBtn.classList.add("is-hidden");
+function onSearch(event) {
+    event.preventDefault();
+    searchQuery = event.currentTarget.elements.searchQuery.value;
+    console.log(searchQuery);
+    fetchImages(searchQuery, page)
+    .then((data) => {
+        // console.log(data.hits);
+        console.log(`totalHits: ${data.totalHits}`);
+        const {hits} = data;
+        hits.map(hit =>{
+            const {
+                webformatURL,
+                largeImageURL,
+                tags, 
+                likes,
+                views, 
+                comments,
+                downloads
+            } = hit;
+        console.log(`webformatURL: ${webformatURL}, largeImageURL: ${largeImageURL}, tags: ${tags}, likes: ${likes}, views: ${views}, comments: ${comments}, downloads: ${downloads}`);
+    });
+        page +=1;
+        refs.loadMoreBtn.classList.remove("is-hidden");
+    })
+    
+       
+}
+refs.form.addEventListener('submit', onSearch);
